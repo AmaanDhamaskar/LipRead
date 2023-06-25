9fab265358fb151c4afd6cf1ae470cc9f3677732
AmaanDhamaskar <100023242+AmaanDhamaskar@users.noreply.github.com>
2023-06-25 21:27:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import av
+import cv2
+from aiortc.contrib.media import MediaRecorder
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+
+
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
+
+    # perform edge detection
+    #img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+def app():
+    def in_recorder_factory() -> MediaRecorder:
+        return MediaRecorder(
+            "input.mp4", format="mp4"
+        )
+
+    def out_recorder_factory() -> MediaRecorder:
+        return MediaRecorder("output.flv", format="flv")
+
+    webrtc_streamer(
+        key="loopback",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={
+            "video": True,
+            "audio": True,
+        },
+        video_frame_callback=video_frame_callback,
+        in_recorder_factory=in_recorder_factory,
+        #out_recorder_factory=out_recorder_factory,
+    )
+
+
+if __name__ == "__main__":
+    app()
